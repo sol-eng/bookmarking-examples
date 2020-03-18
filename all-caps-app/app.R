577758f8e31d343774d43a863397739255e94dc8
kellobri <kellyobriant@gmail.com>
2020-03-18 20:59:25 +0000

    Add three basic bookmarking examples

---
+++ all-caps-app/app.R
@@ -0,0 +1,25 @@
+#
+# The basic single-file bookmarking example on Shiny Dev Center:
+# https://shiny.rstudio.com/articles/bookmarking-state.html
+#
+
+library(shiny)
+
+ui <- function(request) {
+    fluidPage(
+        textInput("txt", "Enter text"),
+        checkboxInput("caps", "Capitalize"),
+        verbatimTextOutput("out"),
+        bookmarkButton()
+    )
+}
+server <- function(input, output, session) {
+    output$out <- renderText({
+        if (input$caps)
+            toupper(input$txt)
+        else
+            input$txt
+    })
+}
+
+shinyApp(ui, server, enableBookmarking = "server")
